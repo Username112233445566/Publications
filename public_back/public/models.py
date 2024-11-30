218e55e8f9ea511e4f4a218d9403cae7da2a6d74
umar1 <casenov2101@gmail.com>
2024-11-30 19:08:54 +0600

    initial commit

--- public_back/public/models.py
+++ public_back/public/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+STARS = (
+    (0, '☆☆☆☆☆'),
+    (1, '⭐'),
+    (2, '⭐⭐'),
+    (3, '⭐⭐⭐'),
+    (4, '⭐⭐⭐⭐'),
+    (5, '⭐⭐⭐⭐⭐'),
+)
+
+
 class Public(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
@@ -13,6 +23,7 @@ class Public(models.Model):
 
 class Comment(models.Model):
     public = models.ForeignKey(Public, on_delete=models.CASCADE)
+    stars = models.IntegerField(choices=STARS, default=0)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
